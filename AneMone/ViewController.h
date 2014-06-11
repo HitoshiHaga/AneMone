af5bdd985a92b1b288118a53310ba560f574939e
HagaHitoshi <haga@gclue.jp>
2014-06-11 19:28:35 +0900

    初回コミット / Schemeをshareに設定

---
+++ AneMone/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  AneMone
+//
+//  Created by HagaHitoshi on 2014/06/11.
+//  Copyright (c) 2014年 GClue, inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ AneMone/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  AneMone
+//
+//  Created by HagaHitoshi on 2014/06/11.
+//  Copyright (c) 2014年 GClue, inc. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
