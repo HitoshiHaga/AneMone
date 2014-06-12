19a1f175adc286605a1b0b84194952c2c6c330c2
HagaHitoshi <haga@gclue.jp>
2014-06-12 16:04:33 +0900

    [AneMoneSubProject]ビルドエラーコードを修正した。

--- AneMoneSubProject/AneMoneSubProject/AppDelegate.m
+++ AneMoneSubProject/AneMoneSubProject/AppDelegate.m
@@ -13,9 +13,6 @@
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
     // Override point for customization after application launch.
-    
-    [UIViewController test];
-    
     return YES;
 }
 							
